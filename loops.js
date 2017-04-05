3ef10425728cb7722fa20ce0e2ab6d3423b3c304
Tom Schaeper <schaeper@hyperverse.com>
2017-04-05 03:47:12 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,12 +1,3 @@
-//array = []
-
-function forLoop(array) {
-  var s
-  for( i = 0;  i < 25; i++) {
-    let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
-    array.push(s)
-  }
-}
 
 function whileLoop(n) {
   while ( n > 0) {
@@ -15,6 +6,23 @@ function whileLoop(n) {
   return 'done'
 }
 
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
 function doWhileLoop(array) {
-  
+  var x
+  do {
+    x = array.pop();
+  } while (array.length > 0 && maybeTrue());
+  return array
+}
+
+function forLoop(array) {
+  var s
+  for( i = 0;  i < 25; i++) {
+    let s = i === 1 ? "I am 1 strange loop." : `I am ${i} strange loops.`
+    array.push(s)
+  }
+  return array
 }
